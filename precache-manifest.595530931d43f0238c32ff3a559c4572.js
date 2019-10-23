65413a127491a038ed3c3b7c54f7edabe7e46534
Oleg Balabukh <olegukoff@gmail.com>
2019-10-23 16:51:45 +0300

    Updates

---
+++ precache-manifest.595530931d43f0238c32ff3a559c4572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f816cb2b58a45666b2ed087f9e7b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eecc4b2d04c4d6df994",
+    "url": "/static/css/2.9f4a86d1.chunk.css"
+  },
+  {
+    "revision": "e93412794930808bf189",
+    "url": "/static/css/main.ce9eb46a.chunk.css"
+  },
+  {
+    "revision": "6eecc4b2d04c4d6df994",
+    "url": "/static/js/2.3f0d7f84.chunk.js"
+  },
+  {
+    "revision": "e93412794930808bf189",
+    "url": "/static/js/main.96011e72.chunk.js"
+  },
+  {
+    "revision": "a5f076de123c8fdfae86",
+    "url": "/static/js/runtime-main.a979e25b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flickr-image-search/precache-manifest.e0425a87bbceeaa08509a209fa9d5d58.js"
+  "/precache-manifest.595530931d43f0238c32ff3a559c4572.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flickr-image-search/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
